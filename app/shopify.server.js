ec34d12c09a02b4592188c5fd587fb790bdb67e4
Abhinash02 <exampaperguide.in@gmail.com>
2026-06-26 16:57:10 +0530

    Hardcode keys to bypass Vercel environment bug

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -7,11 +7,11 @@ import {
 import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
 
 const shopify = shopifyApp({
-  apiKey: process.env.SHOPIFY_API_KEY,
-  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
+  apiKey: process.env.SHOPIFY_API_KEY || "3cc5989af5b0d2caa8d2364b134b291c",
+  apiSecretKey: process.env.SHOPIFY_API_SECRET || "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",
   apiVersion: ApiVersion.October25,
-  scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  scopes: (process.env.SCOPES || "write_products,write_metaobjects,write_metaobject_definitions").split(","),
+  appUrl: process.env.SHOPIFY_APP_URL || "https://shopify-announcement.vercel.app",
   authPathPrefix: "/auth",
   sessionStorage: new MongoDBSessionStorage(
     process.env.MONGO_URI || "mongodb+srv://abhinash:abhinash@cluster0.kvut8us.mongodb.net/announcement_app?retryWrites=true&w=majority&appName=Cluster0",
